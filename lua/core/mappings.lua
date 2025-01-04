0bf2b743e4025e1c98cb834c0bb094021a46eb1b
aleksandr <ugolkov.prog@gmail.com>
2025-01-04 21:35:51 +0200

    init config

---
+++ lua/core/mappings.lua
@@ -0,0 +1,23 @@
+-- Leader
+vim.g.mapleader = " "
+
+-- Insert
+vim.keymap.set("i", "jj", "<Esc>")
+
+-- Navigation
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+
+--NeoTree
+vim.keymap.set('n', '<leader>e', ':Neotree focus<CR>')
+vim.keymap.set('n', '<leader>f', ':Neotree float focus<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+
+-- Tabs
+vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>")
+vim.keymap.set("n", "<s-Tab>", ":BufferLineCyclePrev<CR>")
+vim.keymap.set("n", "<leader>s", ":BufferLinePick<CR>")
+vim.keymap.set("n", "<leader>x", ":BufferLinePickClose<CR>")
+vim.keymap.set("n", "<c-x>", ":BufferLineCloseOthers<CR>")
