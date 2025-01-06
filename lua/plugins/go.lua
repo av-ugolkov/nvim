3e69039998c2bd0956c6bfd9d7f2fc59271ff87e
aleksandr <ugolkov.prog@gmail.com>
2025-01-06 22:50:15 +0200

    update plugins

--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -1,22 +1,42 @@
 return {
-  "ray-x/go.nvim",
-  dependencies = {  -- optional packages
-    "ray-x/guihua.lua",
-    "neovim/nvim-lspconfig",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-		local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
-		vim.api.nvim_create_autocmd("BufWritePre", {
-		pattern = "*.go",
-		callback = function()
-			require('go.format').goimports()
-		end,
-		group = format_sync_grp,
-	})
-    require("go").setup()
-  end,
-  event = {"CmdlineEnter"},
-  ft = {"go", 'gomod'},
-  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+	"ray-x/navigator.lua",
+	dependencies = {
+		{ "hrsh7th/nvim-cmp" }, { "nvim-treesitter/nvim-treesitter" },
+		{ "ray-x/guihua.lua", run = "cd lua/fzy && make" },
+		{
+			"ray-x/go.nvim",
+			event = { "CmdlineEnter" },
+			ft = { "go", "gomod" },
+			build = ':lua require("go.install").update_all_sync()'
+		},
+		{
+			"ray-x/lsp_signature.nvim", -- Show function signature when you type
+			event = "VeryLazy",
+			config = function() require("lsp_signature").setup() end
+		}
+	},
+	config = function()
+		require("go").setup()
+		require("navigator").setup({
+			lsp_signature_help = true, -- enable ray-x/lsp_signature
+			lsp = { format_on_save = true }
+		})
+
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = { "go" },
+			callback = function(ev)
+				-- CTRL/control keymaps
+				vim.api.nvim_buf_set_keymap(0, "n", "<C-i>", ":GoImport<CR>", {})
+				vim.api.nvim_buf_set_keymap(0, "n", "<C-b>", ":GoBuild %:h<CR>", {})
+				vim.api.nvim_buf_set_keymap(0, "n", "<C-t>", ":GoTestPkg<CR>", {})
+				vim.api.nvim_buf_set_keymap(0, "n", "<C-c>", ":GoCoverage -p<CR>", {})
+
+				-- Opens test files
+				vim.api.nvim_buf_set_keymap(0, "n", "A", ":lua require('go.alternate').switch(true, '')<CR>", {}) -- Test
+				vim.api.nvim_buf_set_keymap(0, "n", "V", ":lua require('go.alternate').switch(true, 'vsplit')<CR>", {}) -- Test Vertical
+				vim.api.nvim_buf_set_keymap(0, "n", "S", ":lua require('go.alternate').switch(true, 'split')<CR>", {}) -- Test Split
+			end,
+			group = vim.api.nvim_create_augroup("go_autocommands", { clear = true })
+		})
+	end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,25 +1,26 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = {
-		  'nvim-lua/plenary.nvim',
-		  {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+	'nvim-telescope/telescope.nvim',
+	tag = '0.1.8',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
 	},
 	config = function()
 		require('telescope').setup({
 			extensions = {
 				fzf = {
-					fuzzy = true,                    -- false will only do exact matching
-					override_generic_sorter = true,  -- override the generic sorter
-					override_file_sorter = true,     -- override the file sorter
-					case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-			   }
+					fuzzy = true,    -- false will only do exact matching
+					override_generic_sorter = true, -- override the generic sorter
+					override_file_sorter = true, -- override the file sorter
+					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				}
 			}
 		})
 		require('telescope').load_extension('fzf')
 		local builtin = require('telescope.builtin')
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find Files"})
-		vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "Find Word"})
+		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
+		vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "Find Word" })
 		vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<CR>", {})
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffers'})
+		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find Buffers' })
 	end
 }
