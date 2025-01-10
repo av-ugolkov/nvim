80d8f5670eaa6b62aa0332df25f3023dad6938b2
aleksandr <ugolkov.prog@gmail.com>
2025-01-10 17:28:25 +0200

    nvim-dap-ui

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -9,6 +9,7 @@ vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
 vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", {desc="Clear search highlight"})
 
 --NeoTree
 vim.keymap.set('n', '<leader>e', ':Neotree left focus<CR>')
---
+++ lua/plugins/dap-ui.lua
@@ -0,0 +1,33 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = {
+		"mfussenegger/nvim-dap",
+		"nvim-neotest/nvim-nio",
+	},
+	config = function()
+		local dapui = require('dapui')
+
+		-- Инициализация dap-ui (опционально)
+		dapui.setup({
+			layouts = {
+				{
+					elements = {
+						{ id = "stacks",      size = 0.45 },
+						{ id = "scopes",      size = 0.25 },
+						{ id = "breakpoints", size = 0.25 },
+						{ id = "repl",        size = 0.05 },
+					},
+					size = 40,
+					position = "left",
+				},
+				{
+					elements = {
+						{ id = "console", size = 1 },
+					},
+					size = 10,
+					position = "bottom",
+				},
+			},
+		})
+	end,
+}
