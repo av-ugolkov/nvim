adee3e3c624e07d2aff9527b1cd03ee9ffab874d
aleksandr <ugolkov.prog@gmail.com>
2025-05-26 18:23:33 +0300

    update mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,18 +1,12 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		tag = "v1.10.0",
-		config = function()
-			require('mason').setup()
-		end
-	},
-	{
-		'williamboman/mason-lspconfig.nvim',
-		config = function()
-			require("mason-lspconfig").setup(
-				{
-					ensure_installed = { "lua_ls", "gopls" }
-				})
-		end
-	}
+    "mason-org/mason.nvim",
+    opts = {
+        ui = {
+            icons = {
+                package_installed = "✓",
+                package_pending = "➜",
+                package_uninstalled = "✗"
+            }
+        }
+    }
 }
