703fa790d7afc8dde1a671a2f110078417ac8ad4
aleksandr <ugolkov.prog@gmail.com>
2025-05-26 18:27:51 +0300

    update core configs

--- lua/core/configs.lua
+++ lua/core/configs.lua
@@ -34,14 +34,14 @@ vim.opt.fillchars = {
 -- filetype related
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "gitcommit" },
-	callback = function(ev)
+	callback = function()
 		vim.api.nvim_set_option_value("textwidth", 72, { scope = "local" })
 	end
 })
 
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "markdown" },
-	callback = function(ev)
+	callback = function()
 		vim.api.nvim_set_option_value("textwidth", 0, { scope = "local" })
 		vim.api.nvim_set_option_value("wrapmargin", 0, { scope = "local" })
 		vim.api.nvim_set_option_value("linebreak", true, { scope = "local" })
